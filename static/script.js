d84dcda6c28c82cf596c2988408b7cd067400149
agustinlopez1999 <lopezagustin1999@hotmail.com>
2025-04-09 23:35:01 -0300

    CSS Implementation

--- static/script.js
+++ static/script.js
@@ -11,6 +11,45 @@ document.addEventListener("DOMContentLoaded", () => {
         if (el && url) el.src = url;
     };
 
+    const oldDaysElems = document.getElementsByClassName("coin-days-since-old");
+    for (const el of oldDaysElems) {
+        el.textContent = `${data.days_since_old_data} días`;
+    }
+
+    //format price base on the decimals they have
+    const formatPrice = (price) => {
+        if(price >= 1){
+            return `$${price.toFixed(2)}`;
+        }
+        else{
+            if(price >= 0.01){
+                return `$${price.toFixed(4)}`;
+            }
+            else{{
+                return `$${price.toFixed(6)}`;
+            }}
+        }
+    }
+
+    //cell color base on % change
+    const setChange = (id, value) => {
+        const el = document.getElementById(id);
+        if (!el || value == null || value === undefined) return;
+
+        const num = parseFloat(value);
+        el.textContent = `${num.toFixed(2)}%`;
+
+        el.classList.remove("positive","negative");
+
+        if(num > 0){
+            el.classList.add("positive");
+        } else if (num < 0) {
+            el.classList.add("negative");
+        }
+
+    }
+
+
     //load coins
     fetch("/api/v1/coins/list")
         .then(res=> res.json())
@@ -55,12 +94,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 setImage("coin-image", data.image_large);
 
                 setText("coin-name", data.name);
-                setText("coin-symbol", data.symbol?.toUpperCase());
-                setText("coin-price", `$${data.price?.toFixed(6)}`);
+                setText("coin-symbol", `(${data.symbol?.toUpperCase()})`);
+                setText("coin-price", formatPrice(data.price));
                 setText("coin-marketcap", `$${data.market_cap?.toLocaleString()}`);
                 setText("coin-rank", `#${data.market_cap_rank}`);
 
-                setText("coin-ath", `$${data.ath?.toFixed(6)}`);
+                setText("coin-ath", formatPrice(data.ath));
                 setText("coin-ath-change", `${data.ath_change_percentage?.toFixed(2)}%`);
                 setText("coin-ath-date", new Date(data.ath_date).toLocaleString());
 
@@ -69,17 +108,18 @@ document.addEventListener("DOMContentLoaded", () => {
                 setText("coin-max-supply", data.max_supply?.toLocaleString());
                 setText("coin-infinity", data.max_supply_is_infinity ? "Sí" : "No");
 
-                setText("coin-old-price", `$${data.old_price?.toFixed(6)}`);
+                setText("coin-old-price", formatPrice(data.old_price));
                 setText("coin-old-marketcap", `$${data.old_market_cap?.toLocaleString()}`);
                 setText("coin-old-supply", data.old_circulating_supply?.toLocaleString());
 
                 setText("coin-days-since-old", `${data.days_since_old_data} días`);
                 setText("coin-historical-note", data.historical_note);
 
-                setText("coin-change-24h", `${data.price_change_percentage_24h?.toFixed(2)}%`);
-                setText("coin-change-7d", `${data.price_change_percentage_7d?.toFixed(2)}%`);
-                setText("coin-change-14d", `${data.price_change_percentage_14d?.toFixed(2)}%`);
-                setText("coin-change-30d", `${data.price_change_percentage_30d?.toFixed(2)}%`);
+                setChange("coin-change-24h", data.price_change_percentage_24h);
+                setChange("coin-change-7d", data.price_change_percentage_7d);
+                setChange("coin-change-14d", data.price_change_percentage_14d);
+                setChange("coin-change-30d", data.price_change_percentage_30d);
+
                 setText("coin-change-60d", `${data.price_change_percentage_60d?.toFixed(2)}%`);
                 setText("coin-change-1y", `${data.price_change_percentage_1y?.toFixed(2)}%`);
 
